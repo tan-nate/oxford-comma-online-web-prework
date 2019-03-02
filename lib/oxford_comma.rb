b76ead45132035c7d8a805091c874a52a19c528d
Nate Tan <nktan93@gmail.com>
2019-03-02 19:07:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,5 @@
 require 'pry'
 
-array = ["a", "b", "c"]
-
 def oxford_comma(array)
   if array.length === 1
     array.join
@@ -14,5 +12,3 @@ def oxford_comma(array)
     array.join(", ")
   end
 end
-
-oxford_comma(array)
