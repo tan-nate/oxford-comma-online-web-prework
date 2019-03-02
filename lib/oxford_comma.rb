60984662d6e4c2c90c7d14d7eb50fab2bb6451fa
Nate Tan <nktan93@gmail.com>
2019-03-02 19:06:07 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,19 @@
-def oxford_comma(array)
+require 'pry'
 
+array = ["a", "b", "c"]
+
+def oxford_comma(array)
+  if array.length === 1
+    array.join
+  elsif array.length === 2
+    array.join(" and ")
+  else
+    last_element = array[-1]
+    array.pop
+    array << "and #{last_element}"
+    array.join(", ")
+  end
 end
+
+
+
